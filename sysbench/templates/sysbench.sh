db831e365312de23d4f21647f950dc68635d60b0
Neeky <1721900707@qq.com>
2018-01-08 11:36:54 +0800

    自动化完成sysbench的所有测试用例

--- sysbench/templates/sysbench.sh
+++ sysbench/templates/sysbench.sh
@@ -1,11 +1,11 @@
 sysbench --mysql-host={{ hostIP }} --mysql-port={{ port }} --mysql-user={{ user }} \
   --mysql-password={{ password }} --mysql-db={{ db }} --tables={{ tableCount }} --table_size={{ tableSize }} \
-  --events={{ events }} --threads={{ threads }} {{ opration }} prepare
+  --events={{ events }} --threads={{ threads }} {{ item }} prepare
 
 sysbench --mysql-host={{ hostIP }} --mysql-port={{ port }} --mysql-user={{ user }} \
   --mysql-password={{ password }} --mysql-db={{ db }} --tables={{ tableCount }} --table_size={{ tableSize }} \
-  --events={{ events }} --threads={{ threads }}  {{ opration }} run > /tmp/mysql-z/{{benchCase}}/{{opration}}.log
+  --events={{ events }} --threads={{ threads }}  {{ item }} run > /tmp/mysql-z/{{ config.name }}#{{ config.value }}/{{ item }}.log
 
 sysbench --mysql-host={{ hostIP }} --mysql-port={{ port }} --mysql-user={{ user }} \
   --mysql-password={{ password }} --mysql-db={{ db }} --tables={{ tableCount }} --table_size={{ tableSize }} \
-  {{ opration }} cleanup
+  {{ item }} cleanup
